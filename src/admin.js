a5450789050f2d4b0e5aae91e5f6a77d2c34bbff
sbuteryd@qq.com <sbuteryd@qq.com>
2019-10-01 15:29:58 +0800

    3-5 页面结构开发

--- src/admin.js
+++ src/admin.js
@@ -3,25 +3,24 @@ import {Row,Col} from "antd";
 import Header from './components/Header'
 import Footer from './components/Footer'
 import NavLeft from './components/NavLeft'
+import './style/common.less'
 
 
 class Admin  extends Component{
     render() {
         return (
-            <div>
-                <Row>
-                    <Col span={4}>
-                        <NavLeft/>
-                    </Col>
-                    <Col span={20}>
-                        <Header>Header</Header>
-                        <Row>
-                            Content
-                        </Row>
-                        <Footer>Footer</Footer>
-                    </Col>
-                </Row>
-            </div>
+            <Row className='container'>
+                <Col span={4} className='nav-left'>
+                    <NavLeft/>
+                </Col>
+                <Col span={20} className='main'>
+                    <Header>Header</Header>
+                    <Row className='content'>
+                        Content
+                    </Row>
+                    <Footer>Footer</Footer>
+                </Col>
+            </Row>
         );
     }
 }
--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -3,10 +3,7 @@ import React from "react";
 export default class NavLeft extends React.Component {
     render() {
         return (
-            <div
-                style={{
-                    background:"rgb(42,134,255)"
-                }}>
+            <div>
                 NavLeft
             </div>
         );
