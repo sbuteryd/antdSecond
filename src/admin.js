ae494bf841f5f04937474884d79571bd08cd19bd
sbuteryd@qq.com <sbuteryd@qq.com>
2019-10-01 15:01:43 +0800

    add basic components

--- src/admin.js
+++ src/admin.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react'
 import {Row,Col} from "antd";
 import Header from './components/Header'
 import Footer from './components/Footer'
+import NavLeft from './components/NavLeft'
 
 
 class Admin  extends Component{
@@ -10,7 +11,7 @@ class Admin  extends Component{
             <div>
                 <Row>
                     <Col span={4}>
-                        col-18 col-push-6
+                        <NavLeft/>
                     </Col>
                     <Col span={20}>
                         <Header>Header</Header>
---
+++ src/components/NavLeft/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class NavLeft extends React.Component {
+    render() {
+        return (
+            <div
+                style={{
+                    background:"rgb(42,134,255)"
+                }}>
+                NavLeft
+            </div>
+        );
+    }
+}
