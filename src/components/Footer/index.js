34fee460d75a405ca7cd4df28ea8287d23ba0dc3
sbuteryd@qq.com <sbuteryd@qq.com>
2019-10-02 11:39:17 +0800

    3-10 底部

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './index.less'
 
 export default class Footer extends React.Component {
     render() {
         return (
-            <div>
-                Footer
+            <div className='footer'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid architecto autem,
             </div>
         );
     }
