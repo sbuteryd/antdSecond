b9b468050f74323f694c90280a9f0d37e9441ea2
sbuteryd <sbuteryd@qq.com>
2019-10-07 14:44:55 +0800

    信息确认框

--- src/pages/ui/modals.js
+++ src/pages/ui/modals.js
@@ -59,6 +59,40 @@ class Modals extends React.Component{
             frouth:preState.frouth = false
         }))
     }
+
+
+     info=()=> {
+        Modal.info({
+            title: 'This is a notification message',
+            content: (
+                <div>
+                    <p>some messages...some messages...</p>
+                    <p>some messages...some messages...</p>
+                </div>
+            ),
+            onOk() {},
+        });
+    }
+
+     success=()=>{
+        Modal.success({
+            title: 'This is a success message',
+            content: 'some messages...some messages...',
+        });
+    }
+
+     error=()=> {
+        Modal.error({
+            title: 'This is an error message',
+            content: 'some messages...some messages...',
+        });
+    }
+      warning =()=> {
+        Modal.warning({
+            title: 'This is a warning message',
+            content: 'some messages...some messages...',
+        });
+    }
     render() {
         return (
           <div>
@@ -120,6 +154,12 @@ class Modals extends React.Component{
                       <p>水平垂直居中</p>
                   </Modal>
 
+              </Card>
+              <Card style={{marginTop:30}} title='信心确认框'>
+                  <Button onClick={this.info}>Info</Button>
+                  <Button onClick={this.success}>Success</Button>
+                  <Button onClick={this.error}>Error</Button>
+                  <Button onClick={this.warning}>Warning</Button>
               </Card>
           </div>
         );
